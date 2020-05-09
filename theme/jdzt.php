93fb4b858ed80f3a0366c98bf2dc84852600caf5
jiuduyun <64449053+jiudu-cmd@users.noreply.github.com>
2020-05-09 08:18:20 +0800

    Update jdzt.php

--- theme/jdzt.php
+++ theme/jdzt.php
@@ -11,7 +11,7 @@
     <link rel="shortcut icon" href="<?php echo $_SERVER['base_disk_path'];?>favicon.ico" type="image/x-icon" />
     <style type="text/css">
         body{margin:0;padding:0;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-size:14px;line-height:1em;color:#000;background-color:#F9FAFB;}
-        .topbar{width:100%;height:40px;background-color:#463C6A;}
+        .topbar{width:100%;height:40px;background-color:＃00ae9d;}
         .topbar .login{display:block;line-height:40px;color:#ffffff;margin-left:10%;float:left;}
         .topbar .login ion-icon{display:block;height:40px;line-height:40px;margin-right:5px;float:left;}
         .topbar .operate{display:block;line-height:40px;color:#ffffff;margin-left:10%;float:left;}
